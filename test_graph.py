1160d865489aa5945eb850e83bad22c5998f807a
Emanuele Aina <emanuele.aina@gmail.com>
2006-05-17 21:43:30 +0200

    Test the edge functionality of the Graph class

--- test_graph.py
+++ test_graph.py
@@ -11,5 +11,22 @@ class TestGraph(unittest.TestCase):
         self.failUnlessEqual(len(g.nodes), 1)
         self.failUnlessEqual(g.nodes[0], node)
 
+    def test_edge(self):
+        g = Graph()
+        a = 'a'
+        b = 'b'
+        g.add_node(a)
+        g.add_node(b)
+
+        g.add_edge(a,b)
+
+        outgoing = list(g.outgoing(a))
+        incoming = list(g.incoming(b))
+
+        self.failUnlessEqual(len(outgoing), 1)
+        self.failUnlessEqual(outgoing, incoming)
+        self.failUnlessEqual(outgoing[0].start, a)
+        self.failUnlessEqual(outgoing[0].end,   b)
+
 if __name__ == '__main__':
     unittest.main()
