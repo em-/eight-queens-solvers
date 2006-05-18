61ba9f254ee737741c18ce815acbf2fdbb855dee
Emanuele Aina <emanuele.aina@gmail.com>
2006-05-18 11:13:54 +0200

    Use Graph to keep track of the tree of moves

--- solvers/TreeSolver.py
+++ solvers/TreeSolver.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python
 
 from solvers import Solver
+import graph
 
 class TreeSolver(Solver):
     def __init__(self):
-        self.tree = {}
+        self.tree = graph.Graph()
 
     def set_parent(self, child, parent):
-        self.tree[child] = parent
+        try: 
+            self.tree.add_node(parent)
+            self.tree.add_node(child)
+        except ValueError:
+            pass
+        self.tree.add_edge(child, parent)
+
 
     def get_track(self, goal):
         track = [goal]
-        node = goal
-        while self.tree.has_key(node):
-            node = self.tree[node]
-            track.append(node)
-        return track
+        while True:
+            parents = [e.end for e in self.tree.edges if e.start == track[-1]]
+            if not parents: 
+                return track
+            track.append(parents[0])
